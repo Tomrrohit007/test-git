950fe57c1a6181d215134012b012778c1ed0c8c6
Tomrrohit007 <tomarrohit5034@gmail.com>
2022-12-08 15:37:03 +0530

    y

--- src/History/index.js
+++ src/History/index.js
@@ -10,13 +10,16 @@ const History = props => {
     <li className="list">
       <div className="history-details">
         <p className="time-accessed">{timeAccessed}</p>
+        <div className='app-container'>
         <img src={logoUrl} className="app-icon" alt="domain logo" />
         <div className="title-container">
           <p className="title">{title}</p>
           <p className="domainUrl">{domainUrl}</p>
         </div>
+        </div>
       </div>
-      <button
+      <div>      
+        <button
         type="button"
         className="button"
         onClick={onClickDelete}
@@ -27,7 +30,8 @@ const History = props => {
           className="delete-icon"
           alt="delete icon"
         />
-      </button>
+      </button></div>
+
     </li>
   )
 }
